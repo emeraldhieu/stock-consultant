e1dff21f43a9cbd1706b9ebf9cf5f363867a952c
Emerald Hieu <emerald.hieu@gmail.com>
2019-02-10 22:19:12 +0700

    Remove DynamicFeature
    * That's too much for the simple need
    ** Just check the annotation via ResourceInfo
    * Can't get autowired bean when the filter is registered via FeatureContext
    ** Bug reported https://github.com/resteasy/resteasy-spring-boot/pull/17

--- stock-consultant/src/main/java/com/emeraldhieu/closeprice/ClosePriceValidationFilter.java
+++ stock-consultant/src/main/java/com/emeraldhieu/closeprice/ClosePriceValidationFilter.java
@@ -3,21 +3,36 @@ package com.emeraldhieu.closeprice;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.annotation.Priority;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
 
 import org.springframework.stereotype.Component;
 
 import lombok.extern.apachecommons.CommonsLog;
 
 @Component
+@Provider
 @CommonsLog
+@Priority(1000)
 public class ClosePriceValidationFilter implements ContainerRequestFilter {
+
+    @Context
+    private ResourceInfo resourceInfo;
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        ClosePriceValidator closePriceValidator = resourceInfo.getResourceMethod()
+                .getAnnotation(ClosePriceValidator.class);
+        if (closePriceValidator == null) {
+            return;
+        }
+
         MultivaluedMap<String, String> queryParameters = requestContext.getUriInfo().getQueryParameters();
         // TODO Enrich validation.
         // https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-en/cn/part1/chapter12/server_side_filters.html
--- stock-consultant/src/main/java/com/emeraldhieu/twohundreddma/DmaValidationFilter.java
+++ stock-consultant/src/main/java/com/emeraldhieu/twohundreddma/DmaValidationFilter.java
@@ -6,18 +6,31 @@ import java.time.format.DateTimeFormatter;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
 
 import org.springframework.stereotype.Component;
 
 import lombok.extern.apachecommons.CommonsLog;
 
 @Component
+@Provider
 @CommonsLog
 public class DmaValidationFilter implements ContainerRequestFilter {
 
+    @Context
+    private ResourceInfo resourceInfo;
+
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        DmaValidator dmaValidator = resourceInfo.getResourceMethod()
+                .getAnnotation(DmaValidator.class);
+        if (dmaValidator == null) {
+            return;
+        }
+
         MultivaluedMap<String, String> queryParameters = requestContext.getUriInfo().getQueryParameters();
         // TODO Enrich validation.
         // https://dennis-xlc.gitbooks.io/restful-java-with-jax-rs-2-0-en/cn/part1/chapter12/server_side_filters.html
